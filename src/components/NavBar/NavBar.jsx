4842f7d1ed24d5cfd7780bc1a3e8c4e9e4982c9d
Xavier Dosada <francoxavierdosada@gmail.com>
2022-09-12 18:12:03 -0300

    Menu e-commerce

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+        <h2>
+          <strong>Proveduria</strong>
+        </h2>
+        <ul>
+          <li>Inicio</li>
+          <li>Productos</li>
+          <li>Contacto</li>
+        </ul>
+        
+    </div>
+  );
+}
+
+export default NavBar;
