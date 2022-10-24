c7b86cf4ecc02a858e60f63bbe78ca212eb06aba
Xavier Dosada <francoxavierdosada@gmail.com>
2022-10-24 09:56:05 -0300

    Item Collection II

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 export default function Checkout() {
+  const { orderid } = useParams();
   return (
     <div>
         <h2>Felicitaciones tu compra fue realizada con exito<br/></h2>
+        <h2>{`Este es el ID de tu compra: ${orderid} `}</h2>
     </div>
   )
 }
